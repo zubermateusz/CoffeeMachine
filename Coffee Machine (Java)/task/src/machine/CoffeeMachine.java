a4c49e679c781d12e34a8d475f12f02b74e620db
Mateusz <zubermateusz@gmail.com>
2023-03-12 13:01:55 +0100

    Stage 2/6: Ingredient calculator

--- Coffee Machine (Java)/task/src/machine/CoffeeMachine.java
+++ Coffee Machine (Java)/task/src/machine/CoffeeMachine.java
@@ -1,8 +1,12 @@
 package machine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
     public static void main(String[] args) {
 
+        // Stage 1/6: Making coffee
+        /*
         System.out.println("Starting to make a coffee\n" +
                 "Grinding coffee beans\n" +
                 "Boiling water\n" +
@@ -10,5 +14,14 @@ public class CoffeeMachine {
                 "Pouring coffee into the cup\n" +
                 "Pouring some milk into the cup\n" +
                 "Coffee is ready!");
+        */
+
+        Scanner scanner = new Scanner(System.in);
+        int numbersOfCoffee = scanner.nextInt();
+
+        System.out.println("For "+ numbersOfCoffee +" cups of coffee you will need:\n" +
+                numbersOfCoffee * 200 +" ml of water\n" +
+                numbersOfCoffee * 50 +" ml of milk\n" +
+                numbersOfCoffee * 15+" g of coffee beans");
     }
 }
