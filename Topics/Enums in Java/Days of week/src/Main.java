06f18c387c82d4ae285d7fde9ee36ca373c42e48
Mateusz <zubermateusz@gmail.com>
2023-03-18 16:27:45 +0100

    corrected code style

--- Topics/Enums in Java/Days of week/src/Main.java
+++ Topics/Enums in Java/Days of week/src/Main.java
@@ -2,9 +2,10 @@
 
 public class Main {
 
-    public enum DayOfWeek{
+    public enum DayOfWeek {
         SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
     }
+
     public static void main(String[] args) {
         for (DayOfWeek day : DayOfWeek.values()) {
             System.out.println(day);
